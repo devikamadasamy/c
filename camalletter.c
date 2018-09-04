6e4d12171b8658b048afcc97c54fd1b59f57fadc
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-09-04 22:24:39 +0530

    Create camalletter.c

---
+++ camalletter.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char str[50];
+	int n;
+	gets(str);
+	n=strlen(str);
+
+	
+	for(int i=0;i<n-1;i++)
+	{if(i==0){
+			str[i]=toupper(str[i]);
+		}
+		if(str[i]==' '){
+			str[i+1]=toupper(str[i+1]);
+		}
+		
+	}
+	
+	printf("%s",str);
+	return 0;
+}
