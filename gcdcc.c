32607e727267a5099edb71311c1dbf8707109561
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-09-04 08:52:28 +0530

    Rename gcd.c to gcdcc.c

---
+++ gcdcc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,m,temp;
+	scanf("%d %d",&n,&m);
+	int gcd=1;
+	if(m<n){
+		temp=m;
+		m=n;
+		n=temp;
+	}
+	for(int i=2;i<=n;i++){
+		if(n%i==0 && m%i==0){
+			gcd=i;
+		}
+	}
+	int prod,lcm;
+	prod=n*m;
+	lcm=prod/gcd;
+	printf("%d",lcm);
+	return 0;
+}
