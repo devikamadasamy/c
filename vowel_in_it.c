29055b5104643ed1aa01f8286d049f0619ea4af0
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-09-04 11:29:21 +0530

    Create vowel_in_it.c

---
+++ vowel_in_it.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ #include<string.h>
+int main()
+{
+  char ch;
+  int i=0;
+ char str[20];
+  printf("Input a character\n");
+  scanf("%s", &str);
+  int n=strlen(str);
+  int flag=0;
+ while(i<5){
+ ch=str[i];
+  if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' &&ch <= 'Z')) {
+    if (ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch== 'u' || ch=='U')
+      flag=1;
+      
+  }
+    i=i+1;
+}
+if(flag==1){
+		printf("Yes");
+	}
+	else{
+		printf("No");
+	}
+
+  return 0;
+}
