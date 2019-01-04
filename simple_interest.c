084f4c93edd79f05b8d9626c736c9a734cd45086
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2019-01-04 09:34:35 +0530

    Create simple_interest.c

---
+++ simple_interest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int arr[10],n;
+	
+	for(int i=0;i<3;i++){
+		scanf("%d",&arr[i]);
+	
+	}
+	n=(arr[0]*arr[1]*arr[2])/100;
+	printf("%d",n);
+	return 0;
+}
