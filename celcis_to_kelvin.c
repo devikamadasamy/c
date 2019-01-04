ebb76c952563c54f9808c9b46ac686345773c573
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2019-01-04 09:22:51 +0530

    Create celcis_to_kelvin.c

---
+++ celcis_to_kelvin.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int arr[10],n,k;
+	scanf("%d %d",&n,&k);
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	
+	}
+	printf("%d",arr[k-1]);
+	return 0;
+}
