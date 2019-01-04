d8992bf360f937489d7eb4c9138051c6caf60872
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2019-01-04 09:16:02 +0530

    Update and rename celcis_to_kelvin.c to celcis_to_kelvin_conversion.c

--- celcis_to_kelvin_conversion.c
+++ celcis_to_kelvin_conversion.c
@@ -2,10 +2,10 @@
 
 int main(void) {
 	// your code goes here
-	int kelvin,N;
-	scanf("%d",&N);
+	int kelvin,n;
+	scanf("%d",&n);
 	
-	kelvin=273+N;
+	kelvin=273+n;
 	
 	printf("%d",kelvin);
 	return 0;
