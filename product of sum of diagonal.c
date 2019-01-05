4dae688b6642ee4c8c7a718bb1e07b65c65fa811
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2019-01-05 09:17:01 +0530

    Create product of sum of diagonal.c

---
+++ product of sum of diagonal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int dia1=1,dia2=1,sum=0,arr[20][20],n;
+	scanf("%d",&n);
+	
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	for(int k=0;k<n;k++){
+		dia1*=arr[k][k];
+		dia2*=arr[k][n-k-1];
+	}
+	sum=dia1+dia2;
+	printf("%d",sum);
+	return 0;
+	
+}
